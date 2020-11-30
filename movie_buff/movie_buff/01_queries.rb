a10837beac8c0e7ca838f27aae9d9210e13f5c81
philipk8 <philipk876@gmail.com>
2020-11-30 12:32:56 -0800

    completed 01_queries

--- movie_buff/movie_buff/01_queries.rb
+++ movie_buff/movie_buff/01_queries.rb
@@ -82,4 +82,10 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
+  Movie
+    .select(:title)
+    .where('yr < 1940')
+    .pluck(:title)
+    
+
 end
